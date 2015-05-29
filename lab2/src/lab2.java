9c62e783a55df6ff4cc9ce7567132754701d5d0c
mat brewer <mathewbrewer@mail.weber.edu>
2015-05-28 21:12:03 -0600

    first commit

---
+++ lab2/src/lab2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Owner on 5/28/2015.
+ */
+public class lab2
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+
+    }
+
+
+
+}
---
+++ lab2/src/students.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Owner on 5/28/2015.
+ */
+public class students
+{
+
+
+    protected String firstname;
+    protected String lastname;
+    protected int score;
+    ArrayList<String> greetings = new ArrayList<String>();
+
+
+
+
+
+
+}
